c41d0935a6d75fcfef5cca7adab92c9e11515f40
tenination <tim.ninan@gmail.com>
2017-10-13 16:10:29 -0500

    added multiple app methods

--- client/env/config.example.js
+++ client/env/config.example.js
@@ -3,6 +3,12 @@
 
 // Put your parse application keys here!
 $.ajaxPrefilter(function (settings, _, jqXHR) {
-  jqXHR.setRequestHeader('X-Parse-Application-Id', 'PARSE_APP_ID');
-  jqXHR.setRequestHeader('X-Parse-REST-API-Key', 'PARSE_API_KEY');
+  jqXHR.setRequestHeader('X-Parse-Application-Id', '28f10c64a8b1b900a057b74cabaebaf474573436');
+  jqXHR.setRequestHeader('X-Parse-REST-API-Key', '7543f778eede3b2723e3018977563e69738d7c1b');
 });
+
+
+// ### API Keys:
+//  * Server: http://parse.atx.hackreactor.com/chatterbox/classes/messages
+//  * App ID: 28f10c64a8b1b900a057b74cabaebaf474573436
+//  * API Key: 7543f778eede3b2723e3018977563e69738d7c1b
--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,79 @@
 // YOUR CODE HERE:
+// http://parse.atx.hackreactor.com/chatterbox/classes/messages
+
+// POST requests should be in this format
+var app = {
+  username: 'shawndrost',
+  text: 'trololo',
+  roomname: '4chan'
+};
+
+// var message = {
+//   username: 'Mel Brooks',
+//   text: 'It\'s good to be the king',
+//   roomname: 'lobby'
+// };
+
+app.init = function() {};
+app.send = function(message) {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'http://parse.atx.hackreactor.com/chatterbox/classes/messages',
+    type: 'POST',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+
+
+};
+app.fetch = function() {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: undefined,
+    type: 'GET',
+    data: message,
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+};
+app.clearMessages = function() {
+
+  $('#chats').empty();
+
+};
+
+app.renderMessage = function(message) {
+  var $orig = $('<p></p>');
+  $orig.text(message.text);
+  $('#chats').append($orig);
+
+};
+
+app.renderRoom = function(room) {
+  var $orig = $('<p></p>');
+  $orig.text(room);
+  $('#roomSelect').append($orig);
+
+};
+
+
+
+
+
+
+//Example POST request
